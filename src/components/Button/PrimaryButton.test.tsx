cd5fadf56e22e1e081977c02befd27a1990b74d6
mrcscollett <mrcscollett@gmail.com>
2023-09-15 15:50:51 -0400

    Changes to be committed:

--- src/components/Button/PrimaryButton.test.tsx
+++ src/components/Button/PrimaryButton.test.tsx
@@ -4,10 +4,10 @@ import {render, screen } from '@testing-library/react'
 
 import PrimaryButton from "./PrimaryButton";
 
-describe("Running Test for Marbella Button", () => {
+describe("Running Test for Primary Button", () => {
 
   test("Check Button Disabled", () => {
-    render(<PrimaryButton text="Button marbella" disabled/>)
-    expect(screen.getByRole('button',{name:"Button marbella"})).toBeDisabled();
+    render(<PrimaryButton text="Primary Button" disabled/>)
+    expect(screen.getByRole('button',{name:"Primary Button"})).toBeDisabled();
   });
 });
