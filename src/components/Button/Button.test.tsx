f3be71302588269c452d6b133f977f4f0d2fed60
mrcscollett <mrcscollett@gmail.com>
2023-09-15 14:35:49 -0400

    Changes to be committed: Two components

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Button from "./Button";
+
+describe("Button", () => {
+  test("renders the Button component", () => {
+    render(<Button label="Hello world!" />);
+    expect(<Button label="Hello world!" />).toMatchInlineSnapshot(`
+<Button
+  label="Hello world!"
+/>
+`);
+  });
+});
